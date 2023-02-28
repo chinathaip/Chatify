9455c4c9e5dcbe5ab9492b14d3c44a8e1f43a4dc
chinathaip <cartoonabe@gmail.com>
2023-02-28 17:34:40 +0700

    improvement: separate socket.go

--- router/router.go
+++ router/router.go
@@ -1,11 +1,8 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/chinathaip/chatify/hub"
 	"github.com/chinathaip/chatify/service"
-	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/gorm"
@@ -23,33 +20,4 @@ func RegRoute(h *hub.H, db *gorm.DB) *echo.Echo {
 	e.POST("/messages", handler.handleStoreMessage)
 
 	return e
-}
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-func handleSocket(h *hub.H) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		roomName := c.QueryParam("roomName")
-		if roomName == "" {
-			roomName = "Test Chat Room"
-		}
-
-		conn, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-		if err != nil {
-			return err
-		}
-		defer conn.Close()
-
-		client := hub.NewClient(roomName, conn)
-
-		h.Register <- client
-		h.ReadMsgFrom(client) //read message from client
-		return nil
-	}
 }
---
+++ router/socket.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/chinathaip/chatify/hub"
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleSocket(h *hub.H) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		roomName := c.QueryParam("roomName")
+		if roomName == "" {
+			roomName = "Test Chat Room"
+		}
+
+		conn, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+
+		client := hub.NewClient(roomName, conn)
+
+		h.Register <- client
+		h.ReadMsgFrom(client) //read message from client
+		return nil
+	}
+}
